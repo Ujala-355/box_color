a81b15f4c640564a2290f54543025cfd1ae83bcc
Ujala <ujala21@navgurukul.com>
2023-05-27 12:48:35 +0530

    first

---
+++ box_color.js
@@ -0,0 +1,15 @@
+const box=document.getElementById("id1");
+
+const red=document.getElementById("box1");
+red.onclick=function(){
+    box.style.backgroundColor="red";
+}
+const yellow=document.getElementById("box2");
+yellow.onclick=function(){
+    box.style.backgroundColor="yellow";
+}
+const blue=document.getElementById("box3");
+blue.onclick=function(){
+    box.style.backgroundColor="blue";
+}
+
