6807e5f1eff5943b284cf2fe1b91ef32dd22fc48
Ujala <ujala21@navgurukul.com>
2023-06-07 09:40:04 +0530

    some change

--- box_color.js
+++ box_color.js
@@ -1,8 +1,23 @@
 const box=document.getElementById("id1");
 
+const xhr=new XMLHttpRequest();
 const red=document.getElementById("box1");
 red.onclick=function(){
     box.style.backgroundColor="red";
+    xhr.open("GET", "https://raw.githubusercontent.com/mdn/content/main/files/en-us/_wikihistory.json");
+    xhr.onloadend = postAjaxFunction;
+    xhr.send();
+    
+    
+};
+let postAjaxFunction = function () {
+    if (xhr.status === 200) {
+        box.innerHTML = "Successful";
+        let response = JSON.parse(xhr.responseText);
+        alert(response);
+    } else {
+        box.innerHTML = "Something went wrong.";
+    }
 }
 const yellow=document.getElementById("box2");
 yellow.onclick=function(){
